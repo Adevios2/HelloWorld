dfdb63e3d13923e380ad31abbd1cb15fe141a962
Dostonbek <bmister595@gmail.com>
2023-02-13 22:23:26 +0500

    Initial Commit

---
+++ HelloWorldswiftUI/HelloWorldswiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldswiftUIApp.swift
+//  HelloWorldswiftUI
+//
+//  Created by Dostonbek on 2/13/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldswiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
